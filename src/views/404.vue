5251c82ba3dc377161edf11743674289f42e107c
Flechas Bentley <Flechas.Bentley@gmail.com>
2020-12-09 21:21:27 -0600

    added 404 redirect

---
+++ src/views/404.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="lost">
+        <h2>You seem lost?</h2>
+        <p>Maybe try this:</p>
+        <router-link to="/">Home</router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: '404'
+}
+</script>
+<style lang="scss" scoped>
+.lost{
+    text-align: center;
+}
+</style>
