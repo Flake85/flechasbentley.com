5b20b869651d8783adc8eb2fa7d12d3c93f9ce18
Flechas Bentley <Flechas.Bentley@gmail.com>
2020-12-30 14:37:54 -0600

    test

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    devServer: {
-        host: '0.0.0.0',
-        disableHostCheck: true,
-    },
+  devServer: {
+    host: "0.0.0.0",
+    disableHostCheck: true
+  },
 
-    chainWebpack: config => {
-        config.plugin('html').tap(args => {
-            args[0].title = process.env.VUE_APP_TITLE
-            return args
-        })
-    }
-}
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = process.env.VUE_APP_TITLE;
+      return args;
+    });
+  }
+};
