6c2d4a05dec391df8c6797c86898389fca16ad22
Flechas Bentley <Flechas.Bentley@gmail.com>
2021-01-02 00:46:13 -0600

    used animate.css to animate components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="home p-grid">
     <div class="p-col-8 p-offset-2">
-      <h1 class="animate__animated animate__bounce logotxt">Flechas Bentley</h1>
+      <h1
+        class="animate__animated animate__zoomInDown animate__delay-1s logotxt"
+      >
+        Flechas Bentley
+      </h1>
     </div>
     <div class="p-col-8 p-offset-2">
-      <Card class="p-grid animate__animated animate__zoomIn">
+      <Card class="p-grid animate__animated animate__fadeIn">
         <template #title>
-          Homepage
+          <div class="animate__animated animate__lightSpeedInRight">
+            Homepage
+          </div>
         </template>
         <template #content>
-          Welcome to flechasbentley.com! If you are reading this, then you are
-          here too early. That only means you get to see things as I develop my
-          first website. Be sure to check back for more updates!
+          <div class="animate__animated animate__zoomIn">
+            Welcome to flechasbentley.com! If you are reading this, then you are
+            here too early. That only means you get to see things as I develop
+            my first website. Be sure to check back for more updates!
+          </div>
         </template>
       </Card>
     </div>
