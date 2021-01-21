5ff568a190cf35bdcb023543cf120671d4509823
Flechas Bentley <Flechas.Bentley@gmail.com>
2021-01-21 16:06:06 -0600

    fixed binding type

--- src/directives/scrollanimation.ts
+++ src/directives/scrollanimation.ts
@@ -1,11 +1,12 @@
+import { DirectiveBinding } from "vue";
+
 export default {
-  created(el: HTMLDivElement, binding: any) {
+  created(el: HTMLDivElement, binding: DirectiveBinding) {
     const animatedScrollObserver = new IntersectionObserver(
       (entries, animatedScrollObserver) => {
         entries.forEach(entry => {
           if (entry.isIntersecting) {
             entry.target.classList.add("animate__animated", binding.value);
-            console.log(binding.value);
             animatedScrollObserver.unobserve(entry.target);
           }
         });
