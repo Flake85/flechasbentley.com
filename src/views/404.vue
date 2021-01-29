233ab2e7b438a5053b3234053b47fab16b9f3c80
Flechas Bentley <Flechas.Bentley@gmail.com>
2021-01-29 13:24:57 -0600

    changed bootstrap icons from svg to font

--- src/views/404.vue
+++ src/views/404.vue
@@ -14,19 +14,9 @@
           <li><router-link to="/resume">Resumé</router-link></li>
         </ul>
         <button type="button" class="btn btn-primary" @click="directToHome">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
+          
             class="bi bi-chevron-right animate__animated animate__shakeX"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-            />
-          </svg>
+            
           To Homepage
         </button>
       </div>
