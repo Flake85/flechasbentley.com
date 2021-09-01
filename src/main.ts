3435b424683fb9b87ce991f1d3664f02ffd76db2
Flechas Bentley <Flechas.Bentley@gmail.com>
2021-09-01 13:48:02 -0500

    fixed animate.css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "@/assets/scss/app.scss";
+import "animate.css";
 import "bootstrap";
 import ScrollAnimations from "@/directives/scrollanimation";
 
